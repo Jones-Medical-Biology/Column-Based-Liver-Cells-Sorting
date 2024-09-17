ed1b7c308246fd2d2624c00ea1d0c9950f2bc042
Brian Schildt <schildt.brian@ufl.edu>
2024-09-17 09:40:37 -0400

    minor change

--- 20240209CMNJ FCm Liver/Analysis.R
+++ 20240209CMNJ FCm Liver/Analysis.R
@@ -111,7 +111,7 @@ plot(fcsfiles.FS.GS)
 dev.off()
 
 # 1. Complete the above set of plots according to the gating hierarchy (see
-#    gating_tree.pdf) 
+#    gating_tree.pdf)  
 #
 # 2. Plot everything again but this time change the subset to
 #    "scatter" (i.e.: subset = "scatter")
